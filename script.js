89ac09061f2aab2fbb72d7b224a878ec505fe064
Dwain-Daniel <dwain_90@hotmail.co.uk>
2021-01-23 01:43:40 +0000

    Counting flips on click using jQuery

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+
 $(document).ready(function(){
     
     $(".card").click(function(){
         $(this).addClass("visible");
     });
+
+    $('.card').click(function() {
+    $('#flips').html(function(i, val) { return val*1+1 });
+});
+
 });
